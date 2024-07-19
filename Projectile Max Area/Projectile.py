359ee2fe17866dd478327db88b811f555491ce1f
Sriya PS <174346608+sriya-ps@users.noreply.github.com>
2024-07-19 23:36:42 +0530

    Update

--- Projectile Max Area/Projectile.py
+++ Projectile Max Area/Projectile.py
@@ -6,13 +6,43 @@ global u
 u = 10
 global g
 g = 10
+global values
+values = {}
+global pts
+n = 8
 
 def f(x, theta): 
     return x*np.tan(theta) - g*x**2/(2*u**2*np.cos(theta)**2)
 
 def x_range(x, theta):
     return u**2*np.sin(2*theta)/g
-    
-x = sy.Symbol("x")
-theta = np.arctan(np.sqrt(2))
-print(sy.integrate(f(x, theta), (x, 0, x_range(x, theta))))
+
+def integrating_area(theta):
+    x = sy.Symbol("x")
+    return sy.integrate(f(x, theta), (x, 0, x_range(x, theta)))
+
+for i in range(n,0,-1):
+    theta = (n-i)*np.pi/(2*n)
+    values[theta] = integrating_area(theta)
+print(values)
+area_max = max(values, key= lambda x: values[x])
+print("Angle of projection that gives the maximum area is -", area_max, "radians")
+print("The maximum area is - ", values[area_max], "sq. units")
+
+figure, axes = plt.subplots()
+axes.set_aspect(1)
+plt.xlim(-10,10)
+plt.ylim(-10,10)
+for theta in values.keys():
+    x = np.linspace(-10, 10, 50)
+    y = f(x, theta)
+    if theta==area_max: colour = "r"
+    else: colour = "b"
+    plt.plot(x,y, colour, label = str(theta))
+plt.grid(True, linestyle =':')
+plt.xlim([0, 10])
+plt.ylim([0, 10])
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+ 
+plt.show()
