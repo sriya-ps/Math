931941325a24d40274f5adc8b28ac8b263eb3dd8
Sriya PS <174346608+sriya-ps@users.noreply.github.com>
2024-08-12 22:35:32 +0530

    Update Rotating Liquid.py

--- Rotating Liquid/Rotating Liquid.py
+++ Rotating Liquid/Rotating Liquid.py
@@ -3,29 +3,38 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-c_r = 2 # Radius of container
-c_h = 6 # Height of container
-l_h = 3 # Height of liquid in container
-t = np.linspace(0, 2*np.pi, 24)
-r = np.linspace(0, c_r, 100)
-z = np.linspace(0, c_h, 2)
+r0 = 1 # Radius of container in m
+h0 = 2.5 # Height of liquid in container in m
+H = 4 # Height of container in m
+w = 6 # Angular speed in rad/s
+g = 10 # Gravitation field in m/s/s
 
-# Container
+t = np.linspace(0, 2*np.pi, 36)
+r = np.linspace(0, r0, 100)
+z = np.linspace(0, H, 10)
+
+# Container Cylindrical
 z_c, t_c = np.meshgrid(z, t)
-x_c = c_r * np.cos(t_c)
-y_c = c_r * np.sin(t_c)
+x_c = r0 * np.cos(t_c)
+y_c = r0 * np.sin(t_c)
+
+# Container Plane
+r_p, t_p = np.meshgrid(r, t)
+x_p = r_p * np.cos(t_p)
+y_p = r_p * np.sin(t_p)
+z_p = r_p * 0.0
 
 # Liquid Surface
 r_l, t_l = np.meshgrid(r, t)
 x_l = r_l * np.cos(t_l)
 y_l = r_l * np.sin(t_l)
-z_l = 1 + r_l ** 2
+z_l = h0 + ((w**2)/(2*g))*(r_l**2 - (r0**2/2))
+print(z_l)
 
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 ax.set_title('Rotating Liquid')
-ax.set_axis_off()
+#ax.set_axis_off()
 cyl = ax.plot_wireframe(x_c, y_c, z_c, color="gray", linestyle="dashed")
+pl = ax.plot_wireframe(x_p, y_p, z_p, color="gray", linestyle="dashed")
 sf = ax.plot_surface(x_l, y_l, z_l, cmap=cm.coolwarm, linewidth=0, antialiased=True, cstride=2, rstride=2)
 plt.show()
-
-
